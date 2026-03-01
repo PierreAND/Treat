f4b035b461600a76faa313c01ca3164754e6fe4f
PierreAND <andrieu.ps@gmail.com>
2026-03-01 12:16:12 +0100

    get swager nelmio APi

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
