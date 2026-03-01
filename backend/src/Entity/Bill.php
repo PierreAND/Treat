f81ebf805cfe2fc84074a96ec19a589fa50c4849
PierreAND <andrieu.ps@gmail.com>
2026-03-01 11:09:43 +0100

    full attribute bill

--- backend/src/Entity/Bill.php
+++ backend/src/Entity/Bill.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\BillRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BillRepository::class)]
@@ -13,8 +15,40 @@ class Bill
     #[ORM\Column]
     private ?int $id = null;
 
-    public function getId(): ?int
+    #[ORM\OneToOne(targetEntity: Activity::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Activity $activity = null;
+
+    #[ORM\Column]
+    private ?float $totalAmount = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $drinkType = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\OneToMany(mappedBy: 'bill', targetEntity: BillShare::class, cascade: ['persist', 'remove'])]
+    private Collection $shares;
+
+    public function __construct()
     {
-        return $this->id;
+        $this->shares = new ArrayCollection();
+        $this->createdAt = new \DateTimeImmutable();
     }
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getActivity(): ?Activity { return $this->activity; }
+    public function setActivity(?Activity $activity): static { $this->activity = $activity; return $this; }
+
+    public function getTotalAmount(): ?float { return $this->totalAmount; }
+    public function setTotalAmount(float $totalAmount): static { $this->totalAmount = $totalAmount; return $this; }
+
+    public function getDrinkType(): ?string { return $this->drinkType; }
+    public function setDrinkType(string $drinkType): static { $this->drinkType = $drinkType; return $this; }
+
+    public function getCreatedAt(): ?\DateTimeImmutable { return $this->createdAt; }
+
+    public function getShares(): Collection { return $this->shares; }
 }
