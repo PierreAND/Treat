88632a681ad95afadb022c1dc7a3064397eddf4b
PierreAND <andrieu.ps@gmail.com>
2026-03-04 17:50:12 +0100

    delete usecase logout + change expo for matching with wsl fowarding port

--- frontend/src/application/usecases/user/LogOut-usecase.ts
+++
@@ -1,8 +0,0 @@
-import { AuthRepository } from "@/src/domain/repositories/Auth-Repository";
-
-export class LogOutUseCase{
-    constructor(private authRepository: AuthRepository) {}
-    async execute(): Promise<void>{
-        return this.authRepository.logout()
-    }
-}
