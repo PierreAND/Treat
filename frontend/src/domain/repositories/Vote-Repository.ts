5ba5be653c9b37ba038bccff4f0a10b1412d9d94
PierreAND <andrieu.ps@gmail.com>
2026-03-06 16:53:29 +0100

    add method domain contract

--- frontend/src/domain/repositories/Vote-Repository.ts
+++ frontend/src/domain/repositories/Vote-Repository.ts
@@ -2,5 +2,6 @@ import { VotePayload, VoteResult } from "../entities/vote.model";
 
 export interface VoteRepository {
     submitVotes(activityId: number, votes: VotePayload[]):Promise<void>
-    getResults(activityId: number):Promise<VoteResult[]>
+    getResults(activityId: number):Promise<VoteResult[]>    
+    getVoteStatus(activityId: number): Promise<{ hasVoted: boolean }>;
 }
