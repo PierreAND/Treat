829da869ab0b4dd6f0a8cf3e43574457eda9cad7
PierreAND <andrieu.ps@gmail.com>
2026-03-11 16:29:47 +0100

    add method domain contract for bill displaying

--- frontend/src/domain/repositories/Vote-Repository.ts
+++ frontend/src/domain/repositories/Vote-Repository.ts
@@ -4,4 +4,5 @@ export interface VoteRepository {
     submitVotes(activityId: number, votes: VotePayload[]):Promise<void>
     getResults(activityId: number):Promise<VoteResult[]>    
     getVoteStatus(activityId: number): Promise<{ hasVoted: boolean }>;
+    getVoteCheck(activityId: number): Promise<{allVoted: boolean, votedCount: number, totalCount: number}>
 }
