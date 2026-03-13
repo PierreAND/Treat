eeef9b1817c5f39993b981eb4f1e4d2aab20784c
PierreAND <andrieu.ps@gmail.com>
2026-03-13 16:09:12 +0100

    add logical in vote repository

--- backend/src/Repository/VoteRepository.php
+++ backend/src/Repository/VoteRepository.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Repository;
+
+use App\Entity\Activity;
+use App\Entity\User;
 use App\Entity\Vote;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -9,5 +12,25 @@ class VoteRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Vote::class);
+    }
+
+    public function findByActivity(Activity $activity): array
+    {
+        return $this->findBy(['activity' => $activity]);
+    }
+
+    public function findByActivityAndVoter(Activity $activity, User $voter): array
+    {
+        return $this->findBy(['activity' => $activity, 'voter' => $voter]);
+    }
+
+    public function hasVoted(Activity $activity, User $voter): bool
+    {
+        return count($this->findByActivityAndVoter($activity, $voter)) > 0;
+    }
+
+    public function findAllReceivedByUser(User $user): array
+    {
+        return $this->findBy(['target' => $user]);
     }
 }
