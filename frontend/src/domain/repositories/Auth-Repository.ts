7bb7c8eb81ee1e0e9e2252dca536eea93d1a51dd
PierreAND <andrieu.ps@gmail.com>
2026-03-24 17:10:17 +0100

    add reset & forgot password to domain

--- frontend/src/domain/repositories/Auth-Repository.ts
+++ frontend/src/domain/repositories/Auth-Repository.ts
@@ -4,5 +4,7 @@ export interface AuthRepository {
   login(credentials: LoginPayload): Promise<{ user: User, token: string }>
   register(credentials: RegisterPayload): Promise<{ user: User, token: string }>
   logout(): Promise<void>;
+  forgotPassword(email: string): Promise<void>
+  resetPassword(token: string, password:string): Promise<void>
 
 }
