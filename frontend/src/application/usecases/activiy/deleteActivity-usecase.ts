303f2af5d0cabb48a937c67a159daad91c3b0b20
PierreAND <andrieu.ps@gmail.com>
2026-05-23 20:21:02 +0200

    use case delete architecture

---
+++ frontend/src/application/usecases/activiy/deleteActivity-usecase.ts
@@ -0,0 +1,9 @@
+import { ActivityRepository } from "@/src/domain/repositories/Activity-Repository";
+
+export class DeleteActivityUseCase {
+    constructor (private activityRepository : ActivityRepository) {}
+    async execute() {
+        
+    }
+
+}
